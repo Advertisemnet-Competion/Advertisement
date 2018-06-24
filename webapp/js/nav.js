453fcc0a26e063f8bf803106047e6998a97d55c7
jojoliny <370795317@qq.com>
2018-06-24 18:59:22 +0800

    跳转完成

--- webapp/js/nav.js
+++ webapp/js/nav.js
@@ -1,11 +1,27 @@
 $(function(){
     var nav_child=$(".nav-child");
+    $($(".nav-child")[page]).find(".btn_base").addClass("menu-current");
     $.each(nav_child, function(index,obj) {
         $(this).on("click",function(event){
             //先把class名为menu-current删除
-            $(".menu-current").removeClass("menu-current");
-            $(obj).children(0).addClass("menu-current");
+            /*$(".menu-current").removeClass("menu-current");
+            $(obj).children(0).addClass("menu-current");*/
             //跳到指定页面
+            switch (index){
+                case 0:
+                    window.location.href="./index.html";
+                    break;
+                case 1:
+                    window.location.href="./family.html";
+                    break;
+                case 2:
+                    window.location.href="./society.html";
+                    break;
+                case 3:
+                    window.location.href="./nature.html";
+                    break;
+
+            }
         });
     });
 });
